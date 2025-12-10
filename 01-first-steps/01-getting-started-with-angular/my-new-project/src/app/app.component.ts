99a19e488a0b4ac58e020459ef6f3d3590a55e66
alexbleggijr <alexbleggijr@gmail.com>
2025-12-10 14:30:00 -0300

    inline component structure

--- 01-first-steps/01-getting-started-with-angular/my-new-project/src/app/app.component.ts
+++ 01-first-steps/01-getting-started-with-angular/my-new-project/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonFlatComponent } from './button-flat/button-flat.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 @Component({
   selector: 'app-root',
-  imports: [MyButtonComponent],
+  imports: [MyButtonComponent, ButtonFlatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
