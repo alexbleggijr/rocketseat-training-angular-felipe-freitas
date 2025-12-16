f6e1cf7da75a0fc259e6babbf7ab2bb182194ae7
alexbleggijr <alexbleggijr@gmail.com>
2025-12-16 13:17:00 -0300

    using two-way data binding

--- 01-first-steps/01-getting-started-with-angular/my-new-project/src/app/app.component.ts
+++ 01-first-steps/01-getting-started-with-angular/my-new-project/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ButtonFlatComponent } from './button-flat/button-flat.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 @Component({
   selector: 'app-root',
   imports: [
@@ -10,6 +11,7 @@ import { PropertyBindingComponent } from './property-binding/property-binding.co
     ButtonFlatComponent,
     EventBindingComponent,
     PropertyBindingComponent,
+    TwoWayDataBindingComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
